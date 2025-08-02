e405bbd2b18af91c47a5f5ea479c5e53f25cab05
Alexander Chagua <109567411+alexanderchagua@users.noreply.github.com>
2025-08-02 13:56:14 -0500

    Update conections.php

--- library/conections.php
+++ library/conections.php
@@ -2,14 +2,10 @@
 
 function dataPrueba(){
     $server = 'localhost';
-    $dbname= 'worksafe';
-
-    $username = 'Client';
-    $password = 'QRSu/3T[nWYMlL3W'; 
-
-   /*  $username = 'clients';
-    $password = '9QukwvYTxifZTk@l';  */
-
+    $dbname= 'prueba';
+    $username = 'client';
+    //$password = '9X(meYDO7PdQ)5!9';
+    $password = 'XzTfr.-5_s@02EZ9'; 
     $dsn = "mysql:host=$server;dbname=$dbname";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
@@ -17,10 +13,7 @@ function dataPrueba(){
         $link = new PDO($dsn, $username, $password, $options);
         return $link;
     } catch(PDOException $e) {
-        echo "Error de conexi贸n: " . $e->getMessage();
-        exit; 
+        echo "Error de conexión: " . $e->getMessage();
+        exit; // Termina la ejecución del script si hay un error de conexión
     }
 }
-
-
-?>
